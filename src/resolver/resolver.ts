cbca4b214f1588b7330fb0942ff86016e96848ac
clife537 <chandu537@gmail.com>
2024-08-22 19:08:22 -0700

    misc

--- src/resolver/resolver.ts
+++ src/resolver/resolver.ts
@@ -1,5 +1,6 @@
 
 import { data } from "../data/data.js";
+import { db_connection } from '../data/connections.js';
 
 export const resolvers = {
     Query: {
@@ -8,6 +9,7 @@ export const resolvers = {
       groups: ()=> data.groups,
       categorys: ()=>data.categorys,
       category: (parent: any, args: any, Context: any)=>{
+        db_connection
         return data.categorys.find(category=> category.categor_id === args.categor_id);
       }
     },
